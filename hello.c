4b50615a028a1eeeb03a1df0a4a415e7a742d78f
Pankajkumar2608 <tarachandji104@gmail.com>
2026-01-13 21:28:35 +0530

    push exe file by mistake

--- hello.c
+++ hello.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    // struct - use to create a custom data type that can be used to store multiple values
+    struct hello
+    {
+        int id;
+        char name;
+    };
+    
+    struct hello h;
+    h.id = 1;
+    h.name = 'a';
+    printf("%d %c\n", h.id, h.name);
+    
     printf("Hello world\n");
 }
