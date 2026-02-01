10a70e867be1bbe22bc81bb2ed3e1c5baf80306d
Pankajkumar2608 <tarachandji104@gmail.com>
2026-02-01 17:26:43 +0530

    leared about how to get files and made compress file project

---
+++ rust/project_2/target/debug/compress.d
@@ -0,0 +1,1 @@
+C:\Users\ASUS\Desktop\lowlevel\rust\project_2\target\debug\compress.exe: C:\Users\ASUS\Desktop\lowlevel\rust\project_2\src\main.rs
---
+++ rust/project_2/target/debug/deps/compress-493c9fb94da87c12.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\Desktop\lowlevel\rust\project_2\target\debug\deps\compress-493c9fb94da87c12.d: src\main.rs
+
+C:\Users\ASUS\Desktop\lowlevel\rust\project_2\target\debug\deps\compress-493c9fb94da87c12.exe: src\main.rs
+
+src\main.rs:
